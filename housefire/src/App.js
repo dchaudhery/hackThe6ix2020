fbbc283e0af188d165b30a9a72eb038a0c527254
Zi Ming He <zm2he@uwaterloo.ca>
2020-08-23 02:59:40 -0400

    Refactor layout and update images.

--- housefire/src/App.js
+++ housefire/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 //import BeforeAfterSlider from 'react-before-after-slider';
 import { defineCustomElements as defineImgComparisonSlider } from 'img-comparison-slider/loader';
-import before from './FM2015GE2.JPG';
-import after from './FM2016GE2.JPG';
+import before from './Final2015.JPG';
+import after1 from './Final2016a.JPG';
+import after2 from './Final2016b.JPG';
+import headerpic from './header.JPG';
+import sidebar from './sidebar.JPG';
 import { Button, Grid, Input, Typography } from '@material-ui/core';
 
 defineImgComparisonSlider(window);
@@ -19,44 +22,47 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Grid container direction = "column">
-          <Grid item container direction = "row" style={{ borderBottom: "2px solid #713BDB" }}>
-            <Grid item>
-              <Typography variant="h2" style={{ color: "#713BDB"}}>
-                F I F
-              </Typography>
-            </Grid>
+        <Grid container direction = "row">
+          <Grid item xs={2}>
+            <img src={sidebar}/>
           </Grid>
-          <Grid container item direction = "row">
-            <Grid item xs={7}>
-              { this.state.upload &&
-              <img-comparison-slider>
-                <img slot="after" src={after} width="700" />
-                <img slot="before" src={before} width="700"/>
-              </img-comparison-slider>
-              }
-            </Grid>
-            
-            <Grid item container direction = "column" xs={5} style={{ borderLeft: "2px solid #713BDB" }}>
-              <Grid item><Typography variant="h5" style={{color: "#713BDB"}}>Image Upload</Typography></Grid>
-              <Grid item><label style={{fontSize: "25px"}}>Before Fire<Input type="file" style={{display: "none"}}/></label></Grid>
-              <Grid item><label style={{fontSize: "25px"}}>After Fire<Input type="file" name="after" style={{display: "none"}}/></label></Grid>
-              <Grid item style={{margin: "10px"}}>
-                <Button style={{backgroundColor: "#713BDB"}} className="TButton" onClick={()=>this.setState({ upload: true})}>
-                  Upload
-                </Button>
-              </Grid>
-              <Grid item><Typography variant="h4" style={{color: "#713BDB", borderTop: "2px solid #713BDB"}}>Statistics</Typography></Grid>
-                <Grid container item direction="column" spacing={3} style={{ display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-                <Grid item style={{marginLeft: 25}}>Total Damaged Buildings:{this.state.calculate && 1}</Grid>
-                <Grid item style={{marginLeft: 25}}>Total Safe Buildings:{this.state.calculate && 1}</Grid>
-                <Grid item style={{marginLeft: 25}}>Loss per m<sup>2</sup>: {this.state.calculate && 1}</Grid>
-                <Grid item style={{marginLeft: 25}}>Loss: {this.state.calculate && 1}</Grid>
-                </Grid>
+          <Grid container item direction = "column" xs={8}>
+            <Grid item container direction = "row" style={{ borderBottom: "2px solid #713BDB" }}>
               <Grid item>
-                <Button style={{backgroundColor: "#713BDB"}} onClick={()=>this.setState({calculate: true})}>
-                  Calculate
-                </Button>
+                <img src={headerpic}/>
+              </Grid>
+            </Grid>
+            <Grid container item direction = "row">
+              <Grid item xs={9}>
+                { this.state.upload &&
+                <img-comparison-slider>
+                  <img slot="after" src={this.state.calculate ? after2 : after1} width="700" />
+                  <img slot="before" src={before} width="700"/>
+                </img-comparison-slider>
+                }
+              </Grid>
+              
+              <Grid item container direction = "column" xs={3} style={{ marginBottom: "2px", borderLeft: "2px solid #713BDB" }}>
+                <Grid item><Typography variant="h5" style={{color: "#713BDB"}}>Image Upload</Typography></Grid>
+                <Grid item><label style={{fontSize: "25px"}}>Before Fire<Input type="file" style={{display: "none"}}/></label></Grid>
+                <Grid item><label style={{fontSize: "25px"}}>After Fire<Input type="file" name="after" style={{display: "none"}}/></label></Grid>
+                <Grid item style={{margin: "10px"}}>
+                  <Button style={{color: "white", backgroundColor: "#713BDB"}} className="TButton" onClick={()=>this.setState({ upload: true})}>
+                    Upload
+                  </Button>
+                </Grid>
+                <Grid item><Typography variant="h4" style={{color: "#713BDB", borderTop: "2px solid #713BDB"}}>Statistics</Typography></Grid>
+                  <Grid container item direction="column" spacing={3} style={{ display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                  <Grid item style={{marginLeft: 25}}>Total Damaged Buildings:{this.state.calculate && 1}</Grid>
+                  <Grid item style={{marginLeft: 25}}>Total Safe Buildings:{this.state.calculate && 1}</Grid>
+                  <Grid item style={{marginLeft: 25}}>Loss per m<sup>2</sup>: {this.state.calculate && 1}</Grid>
+                  <Grid item style={{marginLeft: 25}}>Loss: {this.state.calculate && 1}</Grid>
+                  </Grid>
+                <Grid item>
+                  <Button style={{color: "white", backgroundColor: "#713BDB"}} onClick={()=>this.setState({calculate: true})}>
+                    Calculate
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
