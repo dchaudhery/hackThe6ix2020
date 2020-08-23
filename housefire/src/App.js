7eeaa134b4bfff26df6956e9850dcad699effb04
Zi Ming He <zm2he@uwaterloo.ca>
2020-08-23 01:41:48 -0400

    Styling for demo.

--- housefire/src/App.js
+++ housefire/src/App.js
@@ -4,26 +4,66 @@ import './App.css';
 import { defineCustomElements as defineImgComparisonSlider } from 'img-comparison-slider/loader';
 import before from './FM2015GE2.JPG';
 import after from './FM2016GE2.JPG';
+import { Button, Grid, Input, Typography } from '@material-ui/core';
 
 defineImgComparisonSlider(window);
 
-function App() {
+class App extends React.Component {
  // const before = './before.JPG';
   //const after= './after.JPG';
+  constructor(props) {
+    super(props);
+    this.state = { upload: false, calculate: false }
+  }
 
-  return (
-    <div className="App">
-      <img-comparison-slider>
-        <img slot="before" src={before}/>
-        <img slot="after" src={after} />
-      </img-comparison-slider>
-
-      <iframe width="400" height="400" 
-        src="https://www.google.org/crisismap/2016-fort-mcmurray-fire?hl=en&llbox=56.727974%2C56.719473%2C-111.448438%2C-111.481397&t=ROADMAP&layers=5%2C14%2C44%2C41%2C35%2C34%2C11%2C24%2C3%2C2%2C6%3A100&embedded=true"
-        ></iframe>
-
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Grid container direction = "column">
+          <Grid item container direction = "row" style={{ borderBottom: "2px solid #713BDB" }}>
+            <Grid item>
+              <Typography variant="h2" style={{ color: "#713BDB"}}>
+                F I F
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container item direction = "row">
+            <Grid item xs={7}>
+              { this.state.upload &&
+              <img-comparison-slider>
+                <img slot="after" src={after} width="700" />
+                <img slot="before" src={before} width="700"/>
+              </img-comparison-slider>
+              }
+            </Grid>
+            
+            <Grid item container direction = "column" xs={5} style={{ borderLeft: "2px solid #713BDB" }}>
+              <Grid item><Typography variant="h5" style={{color: "#713BDB"}}>Image Upload</Typography></Grid>
+              <Grid item><label style={{fontSize: "25px"}}>Before Fire<Input type="file" style={{display: "none"}}/></label></Grid>
+              <Grid item><label style={{fontSize: "25px"}}>After Fire<Input type="file" name="after" style={{display: "none"}}/></label></Grid>
+              <Grid item style={{margin: "10px"}}>
+                <Button style={{backgroundColor: "#713BDB"}} className="TButton" onClick={()=>this.setState({ upload: true})}>
+                  Upload
+                </Button>
+              </Grid>
+              <Grid item><Typography variant="h4" style={{color: "#713BDB", borderTop: "2px solid #713BDB"}}>Statistics</Typography></Grid>
+                <Grid container item direction="column" spacing={3} style={{ display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                <Grid item style={{marginLeft: 25}}>Total Damaged Buildings:{this.state.calculate && 1}</Grid>
+                <Grid item style={{marginLeft: 25}}>Total Safe Buildings:{this.state.calculate && 1}</Grid>
+                <Grid item style={{marginLeft: 25}}>Loss per m<sup>2</sup>: {this.state.calculate && 1}</Grid>
+                <Grid item style={{marginLeft: 25}}>Loss: {this.state.calculate && 1}</Grid>
+                </Grid>
+              <Grid item>
+                <Button style={{backgroundColor: "#713BDB"}} onClick={()=>this.setState({calculate: true})}>
+                  Calculate
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
